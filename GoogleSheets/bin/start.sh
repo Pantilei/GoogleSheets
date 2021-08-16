d7f5ffd8206e389137bba981df5eb8b7bfdedcfe
pantilei ianulov <ianulov.pantilei@gmail.com>
2021-08-16 22:02:28 +0300

    update

--- GoogleSheets/bin/start.sh
+++ GoogleSheets/bin/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 ROOT=$(dirname $0)/../..
 PYTHON=$ROOT/GoogleSheets/env/bin/python3 
-MODULE=$ROOT 
+MODULE=$ROOT/GoogleSheets/env/bin/uvicorn
 # $PYTHON $MODULE -m GoogleSheets.main
-$PYTHON -m GoogleSheets.main
+$MODULE app:app --host 0.0.0.0 --port 8080
 
