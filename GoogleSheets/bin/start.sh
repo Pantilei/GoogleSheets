63b35ac53a4561b22a2dc2755917ebc54ea72696
pantilei ianulov <ianulov.pantilei@gmail.com>
2021-08-16 19:49:21 +0300

    update

--- GoogleSheets/bin/start.sh
+++ GoogleSheets/bin/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
-ROOT=$(dirname $0)/..
-PYTHON=$ROOT/env/bin/python3 
-MODULE=$ROOT/.. 
+ROOT=$(dirname $0)/../..
+PYTHON=$ROOT/GoogleSheets/env/bin/python3 
+MODULE=$ROOT 
 $PYTHON $MODULE -m GoogleSheets.main
