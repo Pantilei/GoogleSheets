3427fca9600f01a32ef617abc0d95e6c4d829824
pantilei ianulov <ianulov.pantilei@gmail.com>
2021-08-16 23:53:17 +0300

    add delete request

--- GoogleSheets/app.py
+++ GoogleSheets/app.py
@@ -12,14 +12,18 @@ async def root():
 @app.put("/set-value/{row_number}")
 async def set_value(row_number: int):
     sheet_3_first_row_values = spreadsheet.sheet_3.row_values(row_number+1)
-    sheet_2_first_row_values = spreadsheet.sheet_2.row_values(row_number+1)
     # Delete the processed row
     spreadsheet.sheet_3.delete_row(row_number+1)
-    spreadsheet.sheet_2.delete_row(row_number+1)
     # Update status cell
     spreadsheet.update_status_cell()
     return ":".join(sheet_3_first_row_values)
-    # return {
-    #     "sheet_3_first_row": sheet_3_first_row_values,
-    #     "sheet_2_first_row": sheet_2_first_row_values
-    # }
+
+
+@app.delete("/delete-value/{row_number}")
+async def delete_row(row_number: int):
+    sheet_2_first_row_values = spreadsheet.sheet_2.row_values(row_number+1)
+    spreadsheet.sheet_2.delete_row(row_number+1)
+    # Update status cell
+    spreadsheet.update_status_cell()
+
+    return ":".join(sheet_2_first_row_values)
